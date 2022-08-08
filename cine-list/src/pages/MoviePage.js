7a97f53122c3ca84f12793bed3f27ab0e9c00446
Brunu-Mene <bruno.ufes@hotmail.com>
2022-08-08 11:10:09 -0300

    começnado personalização MoviePage

--- cine-list/src/pages/MoviePage.js
+++ cine-list/src/pages/MoviePage.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-import Movie from "../components/Movie"
+import {FaStar, FaRegClock, FaRegCalendarAlt, FaFilm} from 'react-icons/fa'
 import "../style/MoviePage.css"
 
 const api_key = "aaef4efb960f10b9af88cd0e410a1f54";
@@ -19,6 +18,13 @@ const PageFilme = () =>{
         });
     }
 
+    const formatCurrency = (number) =>{
+        return number.toLocaleString("en-US",{
+            style: "currency",
+            currency: "USD",
+        });
+    }
+
     useEffect(() => {
         const movieURL = `https://api.themoviedb.org/3/movie/${id}?&api_key=${api_key}`;
         getMovie(movieURL);
@@ -26,30 +32,37 @@ const PageFilme = () =>{
 
     return(
         <div className="movie-page">
-      {movie && (
-        <>
-            <div className="movie-poster-info">
-                <img src={movie.poster_path ? (IMG_API + movie.poster_path) : 'https://static.thenounproject.com/png/1554490-200.png'} alt={movie.title} />
-                <div className="movie-info">
-                    <h3>{movie.title}</h3>
-                    <span className="nota">{movie.vote_average.toFixed(1)}</span>
+            {movie && (
+            <>
+                    <div className="movie-poster">
+                        <img src={movie.poster_path ? (IMG_API + movie.poster_path) : 'https://static.thenounproject.com/png/1554490-200.png'} alt={movie.title} />
+                    </div>
+                    <div className="movie-title">
+                        <h3>{movie.title}</h3>
+                    </div>
+                    <div className="movie-note">
+                        <h3>IMDb nota:</h3>
+                        <p>{movie.vote_average.toFixed(1)}  <FaStar/></p>
+                    </div>
+                    <div className="info-release">
+                        <h3>Lançado em:</h3>
+                        <p>{movie.release_date} <FaRegCalendarAlt/></p>
+                    </div>
+                    <div className="info-duracao">
+                        <h3>Duração:</h3>
+                        <p>{movie.runtime} minutes <FaRegClock/></p>
+                    </div>
+                    <div className="info-budge">
+                        <h3>Orçamento:</h3>
+                        <p>{formatCurrency(movie.budget)}</p>
+                    </div>
+                <div className="info-sinopse">
+                    <h3>Description:</h3>
+                    <p> <FaFilm/> {movie.overview}</p>
                 </div>
-            </div>
-            <div className="info-diretor">
-                <h3>Directed by:</h3>
-                <p>{movie.crew}</p>
-            </div>
-            <div className="info-duracao">
-                <h3>Duration:</h3>
-                <p>{movie.runtime} minutes</p>
-            </div>
-            <div className="info-sinopse">
-                <h3>Description:</h3>
-                <p>{movie.overview}</p>
-            </div>
-        </>
-      )}
-    </div>
+            </>
+            )}
+        </div>
     );
 };
 
