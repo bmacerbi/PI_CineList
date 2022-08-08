ad529fa98c76cc7c5c8e5588f5334a4d391ee19c
Gustavo D <gdsantolin@gmail.com>
2022-08-08 02:50:15 -0300

    construção da página de um filme

--- cine-list/src/components/Movie.js
+++ cine-list/src/components/Movie.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const Movie = ({poster_path,title,vote_average,id, showLink = true}) => {
+const Movie = ({poster_path,title,vote_average,id}) => {
     return (
     <div className="movie">
-        {showLink && <Link to={`/movie/${id}`}>
+        <Link to={`/movie/${id}`}>
         <img src={poster_path ? (IMG_API + poster_path) : 'https://static.thenounproject.com/png/1554490-200.png'} alt={title} />
         <div className="movie-info">
             <h3>{title}</h3>
             <span className="nota">{vote_average}</span>
         </div>
         
-    </Link>}</div>
+    </Link></div>
     );
 };
 
--- cine-list/src/pages/MoviePage.js
+++ cine-list/src/pages/MoviePage.js
@@ -2,15 +2,54 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Movie from "../components/Movie"
+import "../style/MoviePage.css"
+
+const api_key = "aaef4efb960f10b9af88cd0e410a1f54";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const PageFilme = () =>{
     const { id } = useParams();
-    const [movies, setMovies] = useState([]);
+    const [movie, setMovie] = useState(null);
+  
+    const getMovie = (API) => {
+      fetch(API)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovie(data);
+        });
+    }
+
+    useEffect(() => {
+        const movieURL = `https://api.themoviedb.org/3/movie/${id}?&api_key=${api_key}`;
+        getMovie(movieURL);
+    }, []);
 
     return(
+        <div className="movie-page">
+      {movie && (
         <>
-            <h2>piru</h2>
+            <div className="movie-poster-info">
+                <img src={movie.poster_path ? (IMG_API + movie.poster_path) : 'https://static.thenounproject.com/png/1554490-200.png'} alt={movie.title} />
+                <div className="movie-info">
+                    <h3>{movie.title}</h3>
+                    <span className="nota">{movie.vote_average.toFixed(1)}</span>
+                </div>
+            </div>
+            <div className="info-diretor">
+                <h3>Directed by:</h3>
+                <p>{movie.crew}</p>
+            </div>
+            <div className="info-duracao">
+                <h3>Duration:</h3>
+                <p>{movie.runtime} minutes</p>
+            </div>
+            <div className="info-sinopse">
+                <h3>Description:</h3>
+                <p>{movie.overview}</p>
+            </div>
         </>
+      )}
+    </div>
     );
 };
 
