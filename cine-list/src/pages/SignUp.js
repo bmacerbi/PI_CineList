b987d7e0baece850e62625c42f43870dc3feefcb
Gustavo D <gdsantolin@gmail.com>
2022-08-10 20:00:13 -0300

    consertando frontend login e cadastro

--- cine-list/src/pages/SignUp.js
+++ cine-list/src/pages/SignUp.js
@@ -7,29 +7,31 @@ const SignUp = () =>{
 
     return(
         <>
-        <form className="form-signup">
-            <h3 className="font-signup">Sign Up</h3>
+        <div className="page-signup">
+            <form className="form-signup">
+                <h3 className="font-signup">Sign Up</h3>
 
-            <label className="label-signup" for="username">Username</label>
-            <input className="input-signup" type="text" placeholder="Username" id="username"/>
+                <label className="label-signup" for="username">Username</label>
+                <input className="input-signup" type="text" placeholder="Username" id="username"/>
 
-            <label className="label-signup" for="email">E-mail</label>
-            <input className="input-signup" type="text" placeholder="E-mail" id="email"/>
+                <label className="label-signup" for="email">E-mail</label>
+                <input className="input-signup" type="text" placeholder="E-mail" id="email"/>
 
-            <label className="label-signup" for="phone">Phone Number</label>
-            <input className="input-signup" type="text" placeholder="Phone Number" id="phone"/>
+                <label className="label-signup" for="phone">Phone Number</label>
+                <input className="input-signup" type="text" placeholder="Phone Number" id="phone"/>
 
-            <label className="label-signup" for="password">Password</label>
-            <input className="input-signup" type="password" placeholder="Password" id="password"/>
+                <label className="label-signup" for="password">Password</label>
+                <input className="input-signup" type="password" placeholder="Password" id="password"/>
 
-            <label className="label-signup" for="password-confirm">Confirm Password</label>
-            <input className="input-signup" type="password" placeholder="Password" id="password-confirm"/>
+                <label className="label-signup" for="password-confirm">Confirm Password</label>
+                <input className="input-signup" type="password" placeholder="Password" id="password-confirm"/>
 
-            <Link to="/">
-                <button className="button-register">Register</button>
-            </Link>
-    
-        </form>
+                <Link to="/">
+                    <button className="button-register">Register</button>
+                </Link>
+        
+            </form>
+        </div>
 
         </>
     );
